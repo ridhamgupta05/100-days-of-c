46f645e310a2f466d11fd5aa3c7b9408622c3edd
ridhamgupta05 <ridhamspeaks@gmail.com>
2026-07-10 22:31:03 +0530

    used 'sizeof' operator

---
+++ Concepts/sizeof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    printf("%d", sizeof(a));
+    return 0;
+    /*In this part we have seen the role of 'sizeof' operator.
+    The output is the number of bytes any datatype takes.
+    'sizeof' is an uninary operator not a function.*/
+
+    
+}
