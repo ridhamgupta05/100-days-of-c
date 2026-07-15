386359144e34443909bdcfc0c97f8fdc9c8cb122
ridhamgupta05 <ridhamspeaks@gmail.com>
2026-07-15 08:05:41 +0530

    learned

--- Concepts/modifier_of_int_datatype.c
+++ Concepts/modifier_of_int_datatype.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-    printf("%d", sizeof (int));
-    printf("%d", sizeof (short int));
-    printf("%d", sizeof (long int));
-    printf("%d", sizeof (unsigned int));
-    printf("%d", sizeof (signed int));
-    printf("%d", sizeof (long long int));
-    printf("%d", sizeof (int));
+    printf("size of 'int' datatype-           %d bytes\n", sizeof (int));
+    printf("size of 'short int' datatype-     %d bytes\n", sizeof (short int));
+    printf("size of 'long int' datatype-      %d bytes\n", sizeof (long int));
+    printf("size of 'unsigned int' datatype-  %d bytes\n", sizeof (unsigned int));
+    printf("size of 'signed int' datatype-    %d bytes\n", sizeof (signed int));
+    printf("size of 'long long int' datatype- %d bytes\n", sizeof (long long int));
+    printf("\nNOTE: size of different datatypes deepends on machine's architecture, it can be different for other computers. ");
     return 0;
 
 }
