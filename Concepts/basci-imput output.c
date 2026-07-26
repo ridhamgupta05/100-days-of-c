03bce27ee7bb61069f3b7d267e015c27c7dc81bd
ridhamgupta05 <ridhamspeaks@gmail.com>
2026-07-26 23:35:58 +0530

    learned about I/O function

---
+++ Concepts/basci-imput output.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int a, b, res;
+
+int main()
+{
+    printf("Enter your 1st number: ");
+    scanf("%d", &a);
+
+    printf("Enter your 2nd number: ");
+    scanf("%d", &b);
+
+    res = a + b;
+
+    printf("Sum is: %d\n", res);
+    printf("Ricker: '");
+
+    return 0;
+}
