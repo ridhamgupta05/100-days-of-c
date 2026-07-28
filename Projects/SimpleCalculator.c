6c4f91f46f82566fae2d5a2ad39a6adf1936be63
ridhamgupta05 <ridhamspeaks@gmail.com>
2026-07-28 23:46:15 +0530

    learned about operators and started projects section

---
+++ Projects/SimpleCalculator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+{
+    int num1, num2;
+    printf("Enter your first number: \t");
+    scanf("%d", &num1);
+    printf("Enter your 2nd number: \t\t");
+    scanf("%d", &num2);
+    return 0;
+}
