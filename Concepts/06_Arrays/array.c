d98d5a35dc7c610894bb602ddbde29ad96de2623
ridhamgupta05 <ridhamspeaks@gmail.com>
2026-09-04 00:11:19 +0530

    started pointers

--- Concepts/06_Arrays/array.c
+++ Concepts/06_Arrays/array.c
@@ -2,8 +2,15 @@
 
 int main()
 {
-    int array1[10] = {1,2,3,4,5,6,7,8,9,10};
+    int array0[10] = {1,2,3,4,5,6,7,8,9,10};  //initilization of array.
     for(int i=0; i<=9; i++)
-        printf("%d\n", array1[i]);
+        printf("%d\n", array0[i]);
     return 0;
+
+
+//length of array can be specified by any +ve integer constant expression.
+int arr[5];    //valid
+int arr1[5+5]; //valid
+int arr3[5*3]; //valid
+int arr4[21/3];//valid
 }
